ebd5d8b29fb51e7ebdb79e4328ee347fbe59c973
pahri <pahri@gmail.com>
2026-08-29 14:58:51 +0800

    initial

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module jenkins-zero-downtime
+
+go 1.22.2
---
+++ app/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Response struct {
+	Message  string `json:"message"`
+	Version  string `json:"version"`
+	Hostname string `json:"hostname"`
+}
+
+// func health(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write([]byte(`{"status":"ok"}` + "\n"))
+// }
+
+func health(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "unhealthy", http.StatusInternalServerError)
+}
+
+func main() {
+	version := os.Getenv("APP_VERSION")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
+
+		response := Response{
+			Message:  "Hello",
+			Version:  version,
+			Hostname: hostname,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.HandleFunc("/health", health)
+
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
+
+		response := Response{
+			Version:  version,
+			Hostname: hostname,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	log.Printf("Starting app, version %s", version)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
